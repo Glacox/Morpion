e441bc921078f937d3019f693858636df93c043b
Loris Fiard <lfiard@gaming.tech>
2022-11-17 18:59:32 +0100

    v bc

--- morpion.py
+++ morpion.py
@@ -1,8 +1,9 @@
 def morpion():
     grid = [["-","-","-"], ["-","-","-"], ["-","-","-"]]
     print(grid[0][0] + "  " + grid[0][1] + "  " + grid[0][2] + "\n" + grid[1][0] + "  " + grid[1][1] + "  " + grid[1][2] + "\n" + grid[2][0] + "  " + grid[2][1] + "  " + grid[2][2] + "\n>" )
-    joueurLigne = input("Quelle ligne fdp?\n>")
-    joueurColone = input("Quelle colone fdp?\n>")
-    grid[joueurLigne-1][joueurColone-1] = 1
-    return grid
+    joueurLigne = input("Quelle ligne ?\n>")
+    joueurColone = input("Quelle colone ?\n>")
+    replace = len(grid)
+    for i in range(10):
+        grid[i]
 print(morpion())        
